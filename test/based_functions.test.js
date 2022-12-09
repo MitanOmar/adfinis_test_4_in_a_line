3e791d4d44c29c0ec8e7d302d3649fb91892101b
Mitan Omar <mitan@gmx.ch>
2022-12-09 04:05:44 +0100

    write more test for base functions

--- test/based_functions.test.js
+++ test/based_functions.test.js
@@ -3,10 +3,13 @@ import {isAllElementAreEqual, isThere4ElementsNextToEachOther, isPointExistInBoa
 test('Test Function isAllElementAreEqual', () => {
     expect(isAllElementAreEqual([1, 1, 1, 1])).toBeTruthy();
     expect(isAllElementAreEqual([1, 1, 1, 5])).toBeFalsy();
+    expect(isAllElementAreEqual([1, 1, 2])).toBeFalsy();
 });
 test('Test Function isThere4ElementsNextToEachOther', () => {
     expect(isThere4ElementsNextToEachOther([1, 1, 2, 1, 1, 1, 1])).toBeTruthy()
     expect(isThere4ElementsNextToEachOther([1, 1, 2, 1, 2, 1, 1])).toBeFalsy()
+    expect(isThere4ElementsNextToEachOther([1, 1])).toBeFalsy()
+    expect(isThere4ElementsNextToEachOther([1, 0, 2, 3, 5])).toBeFalsy()
 });
 
 test('check if point exists in boarder', () => {
@@ -19,4 +22,28 @@ test('check if point exists in boarder', () => {
         [0, 0, 0, 0, 0, 0, 0],
     ];
     expect(isPointExistInBoard(gmBoard, 5, 6)).toBeTruthy()
+})
+
+test('check if point exists in boarder', () => {
+    const gmBoard = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ];
+    expect(isPointExistInBoard(gmBoard, 5, 7)).toBeFalsy()
+})
+
+test('check if point exists in boarder', () => {
+    const gmBoard = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ];
+    expect(isPointExistInBoard(gmBoard, 7, 7)).toBeFalsy()
 })
